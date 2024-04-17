119c76ce3ede5ac3291daf88db320163d760b73d
NihatBabayev <nniko6740@gmail.com>
2024-04-17 13:32:21 +0400

    fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from model import SongRecommender
+import json
 
 app = FastAPI()
 recommender = SongRecommender()
 
 @app.post("/getrecommendation/")
 async def hello_person(search_string: str):
-    return recommender.get_top_similar_songs(search_string)
+    result = recommender.get_top_similar_songs(search_string)
+    return json.loads(result)
