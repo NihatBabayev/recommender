a864d3e34a32188eeffeb99f941bc21f3dbc9255
Murad Baghirli <95177453+muradbaghirli@users.noreply.github.com>
2024-04-23 11:39:18 +0400

    Update main.py
    Endpoints both for song and playlist recommendations

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import json
 app = FastAPI()
 recommender = SongRecommender()
 
-@app.post("/getrecommendation/")
-async def hello_person(search_string: str):
-    result = recommender.get_top_similar_songs(search_string)
-    return json.loads(result)
+@app.get("/getsongrecommendations/")
+async def get_song_recommendations(song_query: str):
+    return recommender.get_song_recommendations(song_query)
+
+@app.get("/getplaylistrecommendations/")
+async def get_playlist_recommendations(playlist_url: str):
+    return recommender.get_playlist_recommendations(playlist_url)
