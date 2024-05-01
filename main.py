3eed47471173f9d23d86c8d3d7484a9253fcc712
Murad Baghirli <95177453+muradbaghirli@users.noreply.github.com>
2024-05-01 11:01:24 +0400

    get to post

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = FastAPI()
 recommender = SongRecommender()
 recommendations_router = APIRouter()
 
-@recommendations_router.get("/songs/")
+@recommendations_router.post("/songs/")
 async def get_song_recommendations(song_query: str):
     return recommender.get_song_recommendations(song_query)
 
-@recommendations_router.get("/playlists/")
+@recommendations_router.post("/playlists/")
 async def get_playlist_recommendations(playlist_url: str):
     return recommender.get_playlist_recommendations(playlist_url)
 
